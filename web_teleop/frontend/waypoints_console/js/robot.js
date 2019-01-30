e77d180967a2b4050404021ddcfb0d7ffc18a4d8
Kory Watson <korywat@gmail.com>
2019-01-29 21:31:47 -0800

    Cleanup

--- web_teleop/frontend/waypoints_console/js/robot.js
+++ web_teleop/frontend/waypoints_console/js/robot.js
@@ -1,5 +1,6 @@
 // Connecting to ROS
 // -----------------
+var scene = {}
 var ros = new ROSLIB.Ros();
 
 var movementCommand = null
@@ -24,3 +25,40 @@ ros.on('close', function() {
 
 // Create a connection to the rosbridge WebSocket server.
 ros.connect('ws://shimada.cs.washington.edu:9090');
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    // Scene
+    scene.viewer = new ROS3D.Viewer({
+        divID : 'markers',
+        width : document.body.clientWidth * .4,
+        height : document.body.clientWidth * .4,
+        antialias : true,
+        background : "#f0f0ee"
+    });
+
+    // Grid
+    scene.viewer.addObject(new ROS3D.Grid());
+    
+    // TF Server
+    scene.tfClient = new ROSLIB.TFClient({
+        ros : ros,
+        angularThres : 0.01,
+        transThres : 0.01,
+        rate : 10.0,
+        fixedFrame : '/odom'
+    });
+
+    // Robot Model
+    scene.urdfClient = new ROS3D.UrdfClient({
+       ros : ros,
+       tfClient : scene.tfClient,
+       path : 'static/',
+       rootObject : scene.viewer.scene,
+     });
+
+     setupWaypoints()
+ });
+
+ setupWaypoints = function() {
+     
+ }
