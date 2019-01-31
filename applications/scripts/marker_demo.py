c5dc7dd0e57872d291d9abb2a1124fde8bb568cb
Team 1 Workstation <team1@shimada.com>
2019-01-31 14:49:35 -0800

    Add logic for understanding Kuri current and old location (printing HELLO every 1 meter)

--- applications/scripts/marker_demo.py
+++ applications/scripts/marker_demo.py
@@ -3,20 +3,55 @@
 from visualization_msgs.msg import Marker
 from geometry_msgs.msg import Quaternion, Pose, Point, Vector3
 from std_msgs.msg import Header, ColorRGBA
+from nav_msgs.msg import Odometry
 
 import rospy
 
+class NavPath(object):
+	def __init__(self):
+		initPose = Pose(Point(0, 0, 0), Quaternion(0,0,0,1))
+		self._path = [initPose]
+            
+	def _computeDistance(self, msg):
+		oldLoc = self._path[0].position
+		# TODO: Generalize line to oldLoc = self._path[-1]
+		currLoc = msg.pose.pose.position
+		if (abs(currLoc.x - oldLoc.x) > 1 or (abs(currLoc.y - oldLoc.y) > 1 or abs(currLoc.z - oldLoc.z) > 1)):
+			self._path[0] = msg.pose.pose
+			return True
+		return False
+
+	def callback(self, msg, marker_publisher):
+		rospy.loginfo(msg)
+		hasMovedAway = self._computeDistance(msg)
+		if hasMovedAway: # TODO Replace condition
+			# self._path.append(msg.pose.pose.position) # TODO Uncomment and build up list
+			show_text_in_rviz(marker_publisher, 'HELLO!')
+			# draw_path_in_rviz(marker_publisher, msg.pose.pose)
+		
 def wait_for_time():                                              
     """Wait for simulated time to begin.                          
     """                                                           
     while rospy.Time().now().to_sec() == 0:                       
         pass
 
+def draw_path_in_rviz(marker_publisher, pose):
+    marker = Marker(
+                type=Marker.LINE_STRIP, 
+                id=0,
+                lifetime=rospy.Duration(3),
+                pose=pose,
+                scale=Vector3(0.6, 0.6, 0.6),
+                header=Header(frame_id='base_link'),
+                color=ColorRGBA(1.0, 1.0, 0.5, 0.8),
+                )
+    marker_publisher.publish(marker)
+
 def show_text_in_rviz(marker_publisher, text):
     marker = Marker(
-                type=Marker.TEXT_VIEW_FACING,
+                type=Marker.TEXT_VIEW_FACING, # TODO Change to Marker.LINE_STRIP
                 id=0,
-                lifetime=rospy.Duration(15),
+                lifetime=rospy.Duration(3),
                 pose=Pose(Point(0.5, 0.5, 0.45), Quaternion(0, 0, 0, 1)),
                 scale=Vector3(0.6, 0.6, 0.6),
                 header=Header(frame_id='base_link'),
@@ -26,20 +61,12 @@ def show_text_in_rviz(marker_publisher, text):
 
 def main():
 	rospy.init_node('marker_demo')
+	nav_path = NavPath()
 	marker_publisher = rospy.Publisher('visualization_marker', Marker, queue_size=10)
-	rospy.sleep(0.5)
-	show_text_in_rviz(marker_publisher, 'Hello geoffrey')
-	wait_for_time()
+	# wait_for_time() # TODO Do we need this?
+	rospy.Subscriber('odom', Odometry, nav_path.callback, marker_publisher)
+	rospy.spin()
 
 if __name__ == '__main__':
   main()
-
-class NavPath(object):
-    def __init__(self):
-        self._path = []
-            
-    def callback(self, msg):
-        rospy.loginfo(msg)
-        if SOME_CONDITION:
-            self._path.append(msg.foo.bar)
 
