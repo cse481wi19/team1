cc84c604b1003aea03fb7295b64a91beac4abfb2
Team 1 Workstation <team1@shimada.com>
2019-02-04 13:05:49 -0800

    Add spin call to interactive marker demo (now box shows up) with debug print statements

--- applications/scripts/interactive_marker_demo.py
+++ applications/scripts/interactive_marker_demo.py
@@ -9,49 +9,47 @@ from nav_msgs.msg import Odometry
 
 import rospy
 
-def wait_for_time():                                              
-    """Wait for simulated time to begin.                          
-    """                                                           
-    while rospy.Time().now().to_sec() == 0:                       
-        pass
-
 def handle_viz_input(input):
-    if (input.event_type == InteractiveMarkerFeedback.BUTTON_CLICK):
-        rospy.loginfo(input.marker_name + ' was clicked.')
-    else:
-        rospy.loginfo('Cannot handle this InteractiveMarker event')
+	print('Callback triggered')
+	if (input.event_type == InteractiveMarkerFeedback.BUTTON_CLICK):
+		print('Logging')
+		rospy.loginfo(input.marker_name + ' was clicked.')
+	else:
+		rospy.loginfo('Cannot handle this InteractiveMarker event')
 
 def main():
 	rospy.init_node('interactive_marker_demo')
+	print('Got here')
+	server = InteractiveMarkerServer("simple_marker")
 
-    server = InteractiveMarkerServer("simple_marker")
+	int_marker = InteractiveMarker()
+	int_marker.header.frame_id = "base_link"
+	int_marker.name = "my_marker"
+	int_marker.description = "Simple Click Control"
+	int_marker.pose.position.x = 1
+	int_marker.pose.orientation.w = 1
 
-    int_marker = InteractiveMarker()
-    int_marker.header.frame_id = "base_link"
-    int_marker.name = "my_marker"
-    int_marker.description = "Simple Click Control"
-    int_marker.pose.position.x = 1
-    int_marker.pose.orientation.w = 1
+	box_marker = Marker()
+	box_marker.type = Marker.CUBE
+	box_marker.pose.orientation.w = 1
+	box_marker.scale.x = 0.45
+	box_marker.scale.y = 0.45
+	box_marker.scale.z = 0.45
+	box_marker.color.r = 0.0
+	box_marker.color.g = 0.5
+	box_marker.color.b = 0.5
+	box_marker.color.a = 1.0
 
-    box_marker = Marker()
-    box_marker.type = Marker.CUBE
-    box_marker.pose.orientation.w = 1
-    box_marker.scale.x = 0.45
-    box_marker.scale.y = 0.45
-    box_marker.scale.z = 0.45
-    box_marker.color.r = 0.0
-    box_marker.color.g = 0.5
-    box_marker.color.b = 0.5
-    box_marker.color.a = 1.0
+	button_control = InteractiveMarkerControl()
+	button_control.interaction_mode = InteractiveMarkerControl.BUTTON
+	button_control.always_visible = True
+	button_control.markers.append(box_marker)
+	int_marker.controls.append(button_control)
 
-    button_control = InteractiveMarkerControl()
-    button_control.interaction_mode = InteractiveMarkerControl.BUTTON
-    button_control.always_visible = True
-    button_control.markers.append(box_marker)
-    int_marker.controls.append(button_control)
-
-    server.insert(int_marker, handle_viz_input)
-    server.applyChanges()
+	server.insert(int_marker, handle_viz_input)
+	server.applyChanges()
+	print('Got to the end')
+	rospy.spin()
 
 if __name__ == '__main__':
   main()
