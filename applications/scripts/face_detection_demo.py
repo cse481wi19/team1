afd3e34cfbc161ad5f1b3d6cbf9157fa4f1a130b
Team 1 Workstation <team1@shimada.com>
2019-02-08 17:06:53 -0800

    Created Face Detection Path class and added correct rosmsg types as well as req_mods

--- applications/scripts/face_detection_demo.py
+++ applications/scripts/face_detection_demo.py
@@ -1,28 +1,46 @@
 #!/usr/bin/env python
 
-# from robot_api import Head
-# from robot_api import Lights
-# from robot_api import Vision 
-
 import rospy
 import robot_api
 
-# def on_face_change():
-# Tilt head up or down
-# TODO: change the light color to visualize some aspect of the detections
-# lights = Lights()
-# lights.all_leds(cls, (r,g,b)) 
-# lights.off()
+# Message type of captured vision topic: vision_msgs/ImageClustering
+from vision_msgs.msg import ImageClustering
 
+class FaceChange(object):
+	def __init__(self):
+		self._val = 0
+
+	def _checkFaceBelowOrAbove(self, msg):
+		print('TODO: Implement logic to check whether the face is above or below middle of the frame')
+
+		lights = robot_api.Lights()
+		head = robot_api.Head()
+
+		# lights.all_leds(cls, (r,g,b)) 
+		# lights.off()
+		
+		# if face_below_middle_of_frame:
+		#       tilt_up()
+		# else if face_above_middle_of_frame:
+		#       tilt_down()
+
+	def callback(self, msg):
+		rospy.loginfo(msg)
+		print('Callback reached')
+		# self._checkFaceBelowOrAbove(msg)
+                
 def main():
 	rospy.init_node('face_detection_demo')
 	vision = robot_api.Vision()
+	face_change = FaceChange()
 
 	# TODO: See if a skip ratio is needed for req_mods. (Suggested:3) 
 	vision.req_mods([["activate", "face_detector", {"fps": 6}, {}]], [])
 
-	# rospy.Subscriber('vision/captured', Vision, on_face_change)
-	# rospy.spin()
+	# Trigger callback
+	rospy.Subscriber('vision/captured', ImageClustering, face_change.callback) 
+	
+	rospy.spin()
 
 if __name__ == '__main__':
 	main()
